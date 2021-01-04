17dfd90462ae644a5f6cdc7c53d6b20dd53c5474
Adam <daily.adam@gmail.com>
2021-01-04 11:41:36 -0600

    New addition

---
+++ Chapter 4/Linear Regression Book Examples.R
@@ -0,0 +1,338 @@
+#Setting directory
+
+setwd("C:/Users/daily/Desktop/Repositories/Practical-Machine-Learning-in-R/Chapter 4")
+
+#Loading necessary library and dataset
+
+library(tidyverse)
+
+bikes <- read_csv(file = "C:/Users/daily/Desktop/Repositories/Practical-Machine-Learning-in-R/Chapter 4/Data/bikes.csv", col_types = "Dffffddddd")
+
+#Calculating covariance between humidity and rentals
+
+cov(bikes$humidity, bikes$rentals)
+
+#Calculating standard deviation of humidity and rentals
+
+sd(bikes$humidity)
+
+sd(bikes$rentals)
+
+#Calculating Pearson's correlation of humidity and rentals
+
+pearson <- cov(bikes$humidity, bikes$rentals) / (sd(bikes$humidity) * sd(bikes$rentals))
+
+pearson
+
+#Calculating Pearson's correlation of humidity and rentals using the cor() function instead
+
+cor(bikes$humidity, bikes$rentals)
+
+#Calculating Pearson's correlation between rentals and wind speed and rentals and temperature
+
+cor(bikes$windspeed, bikes$rentals)
+
+cor(bikes$temperature, bikes$rentals)
+
+#Creating correlation plots between all variables
+
+library(corrplot)
+
+bikenumeric <- bikes %>%
+  select(-date)
+
+bike_correlations <- cor(bikenumeric)
+
+corrplot(bike_correlations)
+
+corrplot(bike_correlations, type = "upper")
+
+corrplot.mixed(bike_correlations)
+
+#Finding the coefficient values using OLS methods
+
+B1 <- cov(bikes$temperature, bikes$rentals) / var(bikes$temperature)
+
+B1
+
+B0 <- mean(bikes$rentals) - B1 * mean(bikes$temperature)
+
+B0
+
+#Simple regression model using lm function
+
+bikes_mod1 <- lm(data = bikes, rentals ~ temperature)
+
+bikes_mod1
+
+#Get more info on the model
+
+summary(bikes_mod1)
+
+#Multiple regression model using humidity, wind speed, and temperature to predict rentals
+
+bikes_mod2 <- lm(data = bikes, rentals ~ humidity + windspeed + temperature)
+
+#View the results
+
+summary(bikes_mod2)
+
+#Calculate the model's residual mean
+
+mean(bikes_mod2$residuals)
+
+#Checking normality of residuals
+
+library(olsrr)
+
+ols_plot_resid_hist(bikes_mod2)
+
+#Checking homoscedasticity of residuals
+
+ols_plot_resid_fit(bikes_mod2)
+
+#Checking residual autocorrelation
+
+library(car)
+
+durbinWatsonTest(bikes_mod2)
+
+#Checking for influential points
+
+ols_plot_cooksd_chart(bikes_mod2)
+
+cooks_outliers <- ols_plot_cooksd_chart(bikes_mod2)$outliers
+
+arrange(cooks_outliers, desc(cooks_distance))
+
+#Observe data point 69 to see why it is an outlier
+
+bikes[69, c("rentals", "humidity", "windspeed", "temperature")]
+
+#Look at the summary statistics for the data set not including point 69
+
+summary(bikes[-69, c("rentals", "humidity", "windspeed", "temperature")])
+
+#Look at all the outliers and compare summary statistics to the other data summary
+
+outlier_index <- as.numeric(unlist(cooks_outliers[, "observation"]))
+
+#Summary statistics for outliers
+
+summary(bikes[outlier_index, c("rentals", "humidity", "windspeed", "temperature")])
+
+#Summary statistics for data not including outliers
+
+summary(bikes[-outlier_index, c("rentals", "humidity", "windspeed", "temperature")])
+
+#Summary statistics for the entire data set
+
+summary(bikes[, c("rentals", "humidity", "windspeed", "temperature")])
+
+#Create a new data set without outliers
+
+bikes2 <- bikes[-outlier_index,]
+
+#Checking for multicollinearity
+
+ols_vif_tol(bikes_mod2)
+
+#Creating new variables by squaring them
+
+bikes2 <- bikes2 %>%
+  mutate(humidity2 = humidity^2) %>%
+  mutate(windspeed2 = windspeed^2) %>%
+  mutate(temperature2 = temperature^2)
+
+#Creating a new model with newly transformed predictors
+
+bikes_mod3 <- lm(data = bikes2, rentals ~ humidity + windspeed + temperature + humidity2 + windspeed2 + temperature2)
+
+#View summary statistics
+
+summary(bikes_mod3)
+
+#Removing windspeed2 from model, since it is not significant
+
+bikes_mod3 <- lm(data = bikes2, rentals ~ humidity + windspeed + temperature + humidity2 + temperature2)
+
+#View summary statistics
+
+summary(bikes_mod3)
+
+#View summary of categorical variables
+
+summary(bikes2[, c("season", "holiday", "weekday", "weather")])
+
+library(plyr)
+
+library(dplyr)
+
+#Renaming categorical values
+
+bikes2 <- bikes2 %>%
+  mutate(season = revalue(season, c("1" = "Winter", "2" = "Spring", "3" = "Summer", "4" = "Fall"))) %>%
+  mutate(holiday = revalue(holiday, c("0" = "No", "1" = "Yes"))) %>%
+  mutate(weekday = revalue(weekday, c("0" = "Sunday", "1" = "Monday", "2" = "Tuesday", "3" = "Wednesday", "4" = "Thursday", "5" = "Friday", "6" = "Saturday"))) %>%
+  mutate(weather = revalue(weather, c("1" = "Clear", "2" = "Light precipitation", "3" = "Heavy precipitation")))
+
+#Adding only the season variable to the equation
+
+bikes_mod4 <- lm(data = bikes2, rentals ~ humidity + windspeed + temperature + humidity2 + temperature2 + season)
+
+#View the new model summary statistics
+
+summary(bikes_mod4)
+
+#New model with interaction between windspeed and weather
+
+bikes_mod5 <- lm(data = bikes2, rentals ~ humidity + temperature + humidity2 + temperature2 + season + windspeed * weather)
+
+#View the new model summary statistics
+
+summary(bikes_mod5)
+
+#Separating date column into day, month, and year columns
+
+library(lubridate)
+
+bikes2 <- bikes2 %>%
+  mutate(day = as.numeric(date-min(date))) %>%
+  mutate(month = as.factor(month(date))) %>%
+  mutate(year = as.factor(year(date))) %>%
+  select(-date)
+
+#Stepwise function to pick best model
+
+ols_step_both_p(model = lm(data = bikes2, rentals ~ humidity + weekday + holiday + temperature + humidity2 + temperature2 + season + windspeed * weather +
+                             realfeel + day + month + year), pent = .2, prem = .01, details = FALSE)
+
+#Looking at new model using the selected variables
+
+summary(lm(data = bikes2, rentals ~ windspeed * weather + month + weekday + season + holiday + temperature2 + temperature +
+             year + windspeed + humidity + humidity2))
+
+#CASE STUDY: PREDICTING BLOOD PRESSURE
+
+#Loading the health data
+
+library(tidyverse)
+
+health <- read_csv(file = "C:/Users/daily/Desktop/Repositories/Practical-Machine-Learning-in-R/Chapter 4/Data/health.csv")
+
+#Brief overview of health data
+
+glimpse(health)
+
+#Changing diabetes and smoker from numeric to factor
+
+health %>%
+  mutate(diabetes = as.factor(diabetes)) %>%
+  mutate(smoker = as.factor(smoker))
+
+#Exploring summary data
+
+summary(health)
+
+#Histogram of systolic variable
+
+health %>%
+  ggplot() +
+  geom_histogram(mapping = aes(x = systolic), fill = "lightblue", color = "black")
+
+#Looking at the distribution of the predictor variables
+
+health %>%
+  select(-systolic) %>%
+  keep(is.numeric) %>%
+  gather() %>%
+  ggplot() +
+  geom_histogram(mapping = aes(x = value, fill = key), color = "black") +
+  facet_wrap(~ key, scales = "free")
+
+#Checking correlation between variables
+
+cor(health[,c("systolic", "weight", "height", "bmi", "waist", "age", "fastfood")])
+
+#Building model using age, since it's the highest correlation
+
+health_mod1 <- lm(data = health, systolic ~ age)
+
+#View model summary
+
+summary(health_mod1)
+
+#Fitting multiple regression model
+
+health_mod2 <- lm(data = health, systolic ~ .)
+
+#View summary
+
+summary(health_mod2)
+
+#Test for zero mean of residuals
+
+mean(health_mod2$residuals)
+
+#View histogram of residuals
+
+library(olsrr)
+
+ols_plot_resid_hist(health_mod2)
+
+#Test for heteroskedasticity
+
+ols_plot_resid_fit(health_mod2)
+
+#Test for autocorrelation
+
+library(car)
+
+durbinWatsonTest(health_mod2)
+
+#Check for influential data points
+
+ols_plot_cooksd_chart(health_mod2)
+
+#Check data point 1358 and compare to the entire data set
+
+health[1358,]
+
+summary(health)
+
+#List the outliers
+
+outlier_index <- as.numeric(unlist(ols_plot_cooksd_chart(health_mod2)$outliers[, "observation"]))
+
+outlier_index
+
+#View the summary of outliers
+
+summary(health[outlier_index,])
+
+#Create a new data set excluding outliers
+
+health2 <- health[-outlier_index,]
+
+#Check for multicollinearity
+
+ols_vif_tol(health_mod2)
+
+#Build a new model dropping several of the variables
+
+health_mod3 <- lm(data = health2, systolic ~ weight + age + diabetes)
+
+#View new model summary
+
+summary(health_mod3)
+
+#Create two more predictors
+
+health2 <- health2 %>%
+  mutate(age2 = age^2) %>%
+  mutate(log_age = log(age))
+
+#Building a new model with interactions and non-linear predictors
+
+ols_step_both_p(model = lm(data=health2, systolic ~ weight * diabetes + age * diabetes + age2 * diabetes + log_age * diabetes),
+                pent = .2, prem = .01, details = FALSE)
---
+++ Chapter 4/Linear Regression Book Exercises.R
@@ -0,0 +1,116 @@
+#Setting directory
+
+setwd("C:/Users/daily/Desktop/Repositories/Practical-Machine-Learning-in-R/Chapter 4")
+
+#Loading necessary library and dataset
+
+library(tidyverse)
+
+
+health <- read_csv(file = "C:/Users/daily/Desktop/Repositories/Practical-Machine-Learning-in-R/Chapter 4/Data/health.csv")
+
+#Exercise 2
+
+#Using the blood pressure data set from the use case in this chapter, produce a
+#correlation plot. Use the corrplot.mixed function and generate a plot that shows
+#the correlation coefficients visually above the diagonal and numerically below
+#the diagonal. Provide an interpretaion of your results.
+
+library(corrplot)
+
+health_cor <- cor(health)
+
+corrplot.mixed(health_cor)
+
+#Exercise 3
+
+#You are working with college admission data and trying to determine whether you
+#can predict a student's future GPA based upon their college admission test score.
+#The test is scored on a scale of 0-100, while GPA is measured on a scale of 0.0-4.0.
+#When you build your regression model, you receive the following results:
+
+#Coefficients: Intercept = .695, test = .033
+#Adj. R-squared: .7958
+
+#a) According to this model, what impact would a single point increase in admissions
+#test score have on the prediction of a student's GPA?
+
+#A single point increase in test score would increase the GPA by .033
+
+#b) If a student scored 82 on the test, what would be your prediction of their GPA?
+
+.695 + .033 * 82
+
+#c) If another student scored 97 on the admissions test, what would be your 
+#prediction of their GPA?
+
+.695 + .033 * 97
+
+#d) How well does the model fit the data based upon the Adjusted R-squared?
+
+# The model explains over 79% of the variability in the GPA. This means that the
+#model is a good fit.
+
+#Exercise 4
+
+#Returning to the bicycle rental data set, use R to create a simple regression 
+#model designed to predict the realfell temperature based upon the air temperature.
+#Explain your model and describe how well if fits the data.
+
+bikes <- read_csv(file = "C:/Users/daily/Desktop/Repositories/Practical-Machine-Learning-in-R/Chapter 4/Data/bikes.csv", col_types = "Dffffddddd")
+
+rf_model <- lm(data = bikes, realfeel ~ temperature)
+
+summary(rf_model)
+
+#The model uses temperature to predict the realfeel temperature. With an 
+#Adjusted R-squared of .9834, the model explains over 98% of the variability in temperature.
+
+#Exercise 5
+
+#After building the regression model in exercise 3, you return to the same data set
+#and want to know whether the age of a student at application time is also a contributing
+#factor to their GPA. You add this element to a multiple regression model and receive
+#the results shown here:
+
+#Coefficients: Intercept = -1.900439, test = .025702, age = .182456
+#Adj. R-squared: .9059
+
+#a) According to this model, what impact would a single point increase in admissions
+#test score have on the prediction of a student's GPA? How about a single year
+#increase in age?
+
+#A single point increase in test score would increase the GPA by .025702. A single
+#year increase in age would increase the GPA by .182456.
+
+#b) If a student scored 82 on the admissions test and was 17 years old at the time
+#of application, what would be your prediction of their GPA?
+
+-1.900439 + .025702 * 82 + .182456 * 17
+
+#c) If another student scored 97 on the admissions test and was 19 years old at the 
+#time of application, what would be your prediction of their GPA?
+
+-1.900439 + .025702 * 97 + .182456 * 19
+
+#d) How well does this model fit the data based upon the Adjusted R-squared? How
+#does that compare to the model from exercise 3?
+
+# The model explains over 90% of the variability in the GPA. This means that the
+#model is a very good fit and a better fit than the model from exercise 3.
+
+#Exercise 6
+
+#Returning to the bicycle rental data set, convert your simple regression model
+#from exercise 4 to a multiple regression model that predicts realfeel based upon
+#temperature, windspeed, and humidity. Explain your model and describe how well it
+#fits the data, compared to the model you created in exercise 4.
+
+rf_model2 <- lm(data = bikes, realfeel ~ temperature + windspeed + humidity)
+
+summary(rf_model2)
+
+#This model demonstrates that windspeed also plays a role in realfeel with increasing
+#winds causing realfeel temperature to decrease. The humidity variable is not under
+# the .05 threshold. The Adjusted R-squared is just slightly higher than the value
+#found in exercise 4. This means that this model is a bit better than the previous one.
